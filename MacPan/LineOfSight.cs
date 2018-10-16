0c8c664e49d168f997b0a784fa5b8c687308b053
MansLBS <MansLBS@users.noreply.github.com>
2018-10-16 10:12:09 +0200

    Haha get errors faggot

---
+++ MacPan/LineOfSight.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacPan
+{
+    class LineOfSight
+    {
+        public LineOfSight()
+        {
+
+        }
+
+        public List<Point> LineOfSight(Player player)
+        {
+            if (player.Position.X == Position.X)
+            {
+                if (player.Position.Y > Position.Y)
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+
+            bool rising;
+            float m;
+            float k;
+
+            /*m = (playerY * guardX - playerX * guardY) / (guardX - playerX);
+            k = (guardY - m) / guardX;*/
+
+            return null;
+        }
+    }
+}
